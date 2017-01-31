6181e41432b4a11e6a7dc7b6bb5aae4dcbbbe56e
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2017-01-31 08:36:27 +0000

    bau: Add views for Matching Service and Certificates
    Authors: @richardtowers

--- app/controllers/about_your_service_rp_controller.rb
+++ app/controllers/about_your_service_rp_controller.rb
@@ -10,10 +10,36 @@ class AboutYourServiceRpController < ApplicationController
     session['rp_entity_id'] = entity_id
     session['rp_service_url'] = service_url
 
-    redirect_to '/about-your-service/rp/1'
+    redirect_to '/about-your-service/rp/matching'
   end
 
-  def about_your_service_rp_1
-    render text: "TODO: build this page (rp_entity_id: #{session['rp_entity_id']}, rp_service_url: #{session['rp_service_url']})"
+  def about_your_service_rp_matching
+    render 'about_your_service/rp/matching', :layout => 'application'
+  end
+
+  def about_your_service_rp_matching_post
+    expected_pid = params.fetch('expected-pid')
+    matching_service_entity_id = params.fetch('ms-entity-id')
+    matching_service_private_key = params.fetch('ms-private-key')
+
+    session['rp_expected_pid'] = expected_pid
+    session['rp_matching_service_entity_id'] = matching_service_entity_id
+    session['rp_matching_service_private_key'] = matching_service_private_key
+
+    redirect_to '/about-your-service/rp/certificates'
+  end
+
+  def about_your_service_rp_certificates
+    render 'about_your_service/rp/certificates', :layout => 'application'
+  end
+
+  def about_your_service_rp_certificates_post
+    signing_cert = params.fetch('signing-cert')
+    encryption_cert = params.fetch('encryption-cert')
+
+    session['rp_signing_cert'] = signing_cert
+    session['rp_signing_cert'] = encryption_cert
+
+    render text: 'TODO: the rest of the app'
   end
 end
